1ddba66852a2877e80a6877152ee6ebc2ad21b4d
Martina <albanesemartinax@gmail.com>
2025-04-19 11:04:11 +0200

    setup progetto spring boot

---
+++ elaboratobe2p2/src/main/java/it/ttf/elaboratobe2p2/Elaboratobe2p2Application.java
@@ -0,0 +1,13 @@
+package it.ttf.elaboratobe2p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elaboratobe2p2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elaboratobe2p2Application.class, args);
+	}
+
+}
---
+++ elaboratobe2p2/src/test/java/it/ttf/elaboratobe2p2/Elaboratobe2p2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ttf.elaboratobe2p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elaboratobe2p2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
